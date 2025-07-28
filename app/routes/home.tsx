d385bf57c23872ce6fb6524c7debba6b36c9b150
Husnain <husnainali41940@gmail.com>
2025-07-28 10:54:07 -0700

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "constants";
+import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,6 +15,12 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+    useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
 <section className="main-section">
@@ -21,7 +30,7 @@ export default function Home() {
 </div>
 
 
-{resumes.length > 0 &&(
+{/* {resumes.length > 0 &&(
 <div className="resumes-section">
 {resumes.map((resume)=>{
 <ResumeCard key={resume.id} resume={resume}/>
@@ -29,7 +38,7 @@ export default function Home() {
   })
 }
 </div>
-)}
+)} */}
 
 </section>
   </main>
